f5b7f4d5fb6a3fb1febe8592f7ce62316efaf553
Chris Womack <chriswomack@rev.com>
2021-01-02 03:37:34 -0500

    use correct route for css

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -9,7 +9,7 @@ export default function Admin() {
     (async () => {
       const CMS = (await import('netlify-cms-app')).default;
       CMS.init();
-      CMS.registerPreviewStyle("/global.css");
+      CMS.registerPreviewStyle("/styles/global.css");
       CMS.registerPreviewTemplate('posts', PostPreview);
     })();
   }, []);
